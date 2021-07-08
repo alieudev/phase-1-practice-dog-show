4470358c9ce6b90c21b06c2ec89abcaeaa1d6f64
Alieu Baldeh <alieu.dev@gmail.com>
2021-07-08 11:17:11 -0400

    working solution - upto edit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    dogFetch()
 })
+
+function dogFetch(){
+    fetch('http://localhost:3000/dogs')
+    .then(res => res.json())
+    .then(dogsData => dogsData.forEach(renderDog))
+}
+
+function renderDog(dogsData){
+    let tray = document.createElement('tr')
+    let dogName = document.createElement('td')
+    let dogBreed = document.createElement('td')
+    let dogSex = document.createElement('td')
+    let editBtn = document.createElement('button')
+    
+    editBtn.type = 'submit'
+    editBtn.textContent = 'Edit Dog'
+    dogName.textContent = dogsData.name
+    dogBreed.textContent = dogsData.breed
+    dogSex.textContent = dogsData.sex
+    
+    tray.append(dogName, dogBreed, dogSex, editBtn)
+
+    let table = document.querySelector('#table-body')
+    table.append(tray)
+}
